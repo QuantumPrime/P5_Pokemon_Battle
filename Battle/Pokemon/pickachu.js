fc97e9896d05e85dcdb2c5555656ad2f053a349f
QuantumPrime <38193615+QuantumPrime@users.noreply.github.com>
2018-04-09 10:29:29 -0500

    Added all calculations for stats

--- Battle/Pokemon/pickachu.js
+++ Battle/Pokemon/pickachu.js
@@ -4,11 +4,27 @@ class Pickachu{
         this.pokName = "Pikachu"
         this.HealthEV = 0;
         this.HealthIV = 0;
+
         this.AttackEV = 0;
         this.AttackIV = 0;
+
+        this.DefenceEV = 0;
+        this.DefenceIV = 0;
+
+        this.SpecialEV = 0;
+        this.SpecialIV = 0;
+
+        this.SpeedEV = 0;
+        this.SpeedIV = 0;
+
+
         this.level = 1;
         this.baseHealth = 35;
         this.baseAttack = 55
+        this.baseDef = 30;
+        this.baseSpAtk = 50;
+        this.baseSpDef = 40;
+        this.baseSpeed = 90;
 
         this.calcStats();
        
@@ -46,6 +62,7 @@ class Pickachu{
 
     calcStats(){
         //https://cdn.bulbagarden.net/upload/d/d4/HP_calc.png
+        //Health Calculations
         this.maxHealth1 = this.baseHealth + this.HealthIV;
         this.maxHealth2 = sqrt(this.HealthEV)/4;
         this.maxHealth = ((this.maxHealth1 * 2 + this.maxHealth2 * this.level)/100) + this.level + 10;
@@ -53,15 +70,40 @@ class Pickachu{
         //console.log(this.maxHealth);
 
         //https://cdn.bulbagarden.net/upload/d/d6/Statcalc_gen12.png
+        //attack Calculations
         this.attackCalc1 = this.baseAttack + this.AttackIV;
         this.attackCalc2 = sqrt(this.HealthEV)/4;
         this.attack = (((this.attackCalc1 * 2 + this.attackCalc2) * this.level)/100) + 5;
         this.attack = Math.round(this.attack);
-        console.log(this.attack);
+        //console.log("Attack " + this.attack);
+
+        //Defence Calculations
+        this.DefCalc1 = this.baseDef + this.DefenceIV;
+        this.DefCalc2 = sqrt(this.DefenceEV)/4;
+        this.defence = (((this.DefCalc1 * 2 + this.DefCalc2) * this.level)/100) + 5;
+        this.defence = Math.round(this.defence);
+        //console.log("Defence " + this.defence);
+
+        this.SpAtkCalc1 = this.baseSpAtk + this.SpecialIV;
+        this.SpAtkCalc2 = sqrt(this.SpecialEV)/4;
+        this.SpAtack = (((this.DefCalc1 * 2 + this.DefCalc2) *this.level)/100) + 5;
+        
+        //Special Defence Calculations
+        this.SpDefCalc1 = this.baseSpDef + this.SpecialIV;
+        this.SpDefCalc2 = sqrt(this.SpecialIV)/4;
+        this.SpDef = (((this.SpDefCalc1 * 2 + this.DefCalc2) * this.level)/100) + 5;
+        this.SpDef = Math.round(this.SpDef);
+        //console.log("Special Defence " + this.SpDef);
+
+        //Speed Calculations
+        this.SpeedCalc1 = this.baseSpeed + this.SpeedIV;
+        this.SpeedCalc2 = sqrt(this.SpeedEV)/4;
+        this.Speed = (((this.SpeedCalc1 * 2 + this.SpeedCalc2) * this.level)/100) + 5;
+        this.Speed = Math.round(this.Speed);
+        //console.log("Speed " + this.Speed);
     }
 
     moveDamage(move){
-
         if(move == "Tackle"){
             if(allmoves.Tackle.attackType == this.pokeType){
                 stabMod = 1.5;
@@ -76,11 +118,12 @@ class Pickachu{
             if(allmoves.ThunderShock.attackType == this.pokeType){
                 stabMod = 1.5;
             }else{
-                stabMod = 1.5;
+                stabMod = 1;
             }
             attackPower = allmoves.ThunderShock.Pow;
         }
     }
+
     movePP(move, sub){
         if(move == "Tackle"){
             attackPP = allmoves.Tackle.PP - sub;
