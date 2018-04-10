caf59b382673b5c11d92aa112ce19aeea3748391
QuantumPrime <38193615+QuantumPrime@users.noreply.github.com>
2018-04-10 18:55:23 -0500

    Update attack.js

--- Battle/attack.js
+++ Battle/attack.js
@@ -4,12 +4,12 @@ function Attack(){
 	this.fulldamage = 0;
 
 
-	this.attackProp = function(attack, attackPow, pokLevel, pokAttack, oppDef, modifier){
+	this.attackProp = function(attackPow, pokLevel, pokAttack, oppDef, modifier){
 		this.damage1 = ((2*pokLevel)/5) + 2;
 		this.damage2 = attackPow * pokAttack/oppDef;
 		this.fulldamage = (((this.damage1 * this.damage2)/(50)) + 2) * modifier;
 		this.fulldamage = Math.round(this.fulldamage)
-		//console.log(this.fulldamage);
+		console.log(this.fulldamage);
 		attack = this.fulldamage;
 	}
 }
