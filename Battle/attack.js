dd29db92447efa70579fe919f54b8652f5f1f472
QuantumPrime <38193615+QuantumPrime@users.noreply.github.com>
2018-04-10 17:41:42 -0500

    Update attack.js

--- Battle/attack.js
+++ Battle/attack.js
@@ -1,7 +1,4 @@
 function Attack(){
-	//this.pokemon = new Pickachu;
-	//this.allmoves = new allMoves;
-	
 	this.damage1 = 0;
 	this.damage2 = 0;
 	this.fulldamage = 0;
@@ -12,7 +9,7 @@ function Attack(){
 		this.damage2 = attackPow * pokAttack/oppDef;
 		this.fulldamage = (((this.damage1 * this.damage2)/(50)) + 2) * modifier;
 		this.fulldamage = Math.round(this.fulldamage)
-		console.log(this.fulldamage);
+		//console.log(this.fulldamage);
 		attack = this.fulldamage;
 	}
 }
